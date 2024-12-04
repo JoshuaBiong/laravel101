77ec55838fa0e0a467830a27af5253586c941607
Joshua Biong <joshuabiong7@gmail.com>
2024-12-04 11:59:12 +0800

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+About Page 
+    </x-slot:heading>
+   <h1>This is from About page</h1>
+  </x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <x-slot:heading>
+    Home Page
+  </x-slot:heading>
+ <h1>This is from Home page</h1>
+</x-layout>
